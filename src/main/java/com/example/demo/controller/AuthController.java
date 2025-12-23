e07d0f57c6288ceb9b9c55f06d7522504edadef0
bala-amypo <bala@amypo.in>
2025-12-23 03:57:35 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,28 +1,43 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.LoginRequest;       
-import com.example.demo.dto.RegisterRequest;    
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.model.AppUser;
+import com.example.demo.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@PostMapping("/register")
-@Operation(summary = "Register new user")
-public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-    
-    if (userRepository.findByEmail(request.getEmail()).isPresent()) {
-        return ResponseEntity.badRequest().body("Email already exists!");
+@RequestMapping("/api/auth")
+@Tag(name = "Authentication APIs")
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    AppUser newUser = new AppUser();
-    newUser.setEmail(request.getEmail());
-    newUser.setPassword(request.getPassword());
-    newUser.setRole(request.getRole()); 
-    
-   
-    userRepository.save(newUser);
+    @PostMapping("/register")
+    @Operation(summary = "Register new user")
+    public ResponseEntity<String> register(
+            @RequestBody RegisterRequest request) {
 
-    return ResponseEntity.ok("User registered successfully!");
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Email already exists!");
+        }
+
+        AppUser newUser = new AppUser();
+        newUser.setEmail(request.getEmail());
+        newUser.setPassword(request.getPassword());
+        newUser.setRole(request.getRole());
+
+        userRepository.save(newUser);
+
+        return ResponseEntity.ok("User registered successfully!");
+    }
 }
