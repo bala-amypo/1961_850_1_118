53947e119d8d953f4baccfab23de060aff3add8a
bala-amypo <bala@amypo.in>
2025-12-26 09:15:56 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,22 +1,30 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
-    private String email;     
+    
+    @NotBlank
+    private String username;
+    
+    @NotBlank
     private String password;
 
-   
     public LoginRequest() {}
 
-  
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public String getUsername() { 
+        return username; 
+    }
+    
+    public void setUsername(String username) { 
+        this.username = username; 
     }
 
-   
-    public String getEmail() { return email; } 
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() { 
+        return password; 
+    }
+    
+    public void setPassword(String password) { 
+        this.password = password; 
+    }
 }
