5ceba815a9f5e79c845d9b17fd6db273340c3db7
bala-amypo <bala@amypo.in>
2025-12-26 06:28:50 +0000

    User commit

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -13,6 +13,10 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(unique = true, length = 50)
+    private String username;
+
     @Email
     @NotBlank
     @Column(unique = true, length = 100)
@@ -22,15 +26,17 @@ public class AppUser {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    private String role;
+    private Role role;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     public AppUser() {}
 
-    public AppUser(String email, String password, String role) {
+    public AppUser(String username, String email, String password, Role role) {
+        this.username = username;
         this.email = email;
         this.password = password;
         this.role = role;
@@ -45,14 +51,17 @@ public class AppUser {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
 
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
